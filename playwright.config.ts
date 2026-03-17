fa078e43051106853bead053a652a8198758cbfb
JSMark11 <johnmark.salbilla20@gmail.com>
2026-03-17 23:35:21 +0800

    updating playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,42 +12,90 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: './tests',
+
+  //Seting up timeout for each test steps.
+  timeout: 30_000, // 30 seconds
+  //Seting up global timeout for each test.
+  globalTimeout: 90_000, // 90 seconds
+
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL:'https://betterlaspinas.org/',
+    testIdAttribute: 'data-test',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    //Allows you to collect trace for all tests. Options are "on", "off", "retain-on-failure", "on-first-retry"
+    trace: "on",
+
+    //Timeout for each action like click(), fill() etc.
+    actionTimeout: 0, 
+
+    //Ignore HTTPS errors, such as self-signed certificates. Useful for testing in development environments.
+    ignoreHTTPSErrors: true, 
+
+    //Record video only when a test fails. Videos are saved in the "test-results" directory.
+    video: "retain-on-failure",
+
+    //Take a screenshot only when a test fails. Screenshots are saved in the "test-results" directory.
+    screenshot: "only-on-failure",
+
+    //Run tests in headless mode. Set to false if you want to see the browser during test execution.
+    headless: true,
+
   },
 
   /* Configure projects for major browsers */
   projects: [
+   {
+      name: "setup", //a setup project to run setup files
+      testMatch: /.*\.setup.ts/, // only runs setup files
+    },
+    
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      //ensures setup project runs before this one, allowing us to set up the environment (e.g., log in) before running tests that require it.
+      dependencies: ["setup"], 
+
+      //grants clipboard read permission to the tests running in this project, allowing them to access the clipboard content during test execution.
+      use: { ...devices["Desktop Chrome"], permissions: ["clipboard-read"] }, 
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+
+
+    // {
+    //   name: 'firefox',
+    //   //ensures setup project runs before this one, allowing us to set up the environment (e.g., log in) before running tests that require it.
+    //   dependencies: ["setup"],
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   //ensures setup project runs before this one, allowing us to set up the environment (e.g., log in) before running tests that require it.
+    //   dependencies: ["setup"],
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
